f27fddfd1fe73ddce22db790ae218abb33c965d1
Don <serfdon@gmail.com>
2025-05-20 11:09:45 -0700

    Changed Font

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ mynumbers.colors = {
 	{"yellow",     "Yellow",     "#e3ff00"},
 }
 
-local num = {"0","1","2","3","4","5","6","7","8","9","plus","minus","multiply","divide","equals","bracket_r","bracket_l","slash","question","blank"}
+local num = {"0","1","2","3","4","5","6","7","8","9","plus","minus","multiply","divide","equals","bracket_r","bracket_l","square_bracket_r","square_bracket_l","curly_bracket_r","curly_bracket_l","back_slash","forward_slash","blank","ampersand","apostrophe","asterisk","at","caret","colon","dollar","exclamation","hash","comma","percent","period","question_mark","quote","semi_colon","tilde"}
 
 for _, a in pairs(num) do
 
@@ -40,7 +40,12 @@ if core.get_modpath("mypaint") then
 	local paintables = {
 		"mynumbers:0","mynumbers:1","mynumbers:2","mynumbers:3","mynumbers:4","mynumbers:5","mynumbers:6","mynumbers:7",
 		"mynumbers:8","mynumbers:9","mynumbers:plus","mynumbers:minus","mynumbers:multiply","mynumbers:divide","mynumbers:equals",
-		"mynumbers:bracket_r","mynumbers:bracket_l","mynumbers:slash","mynumbers:question","mynumbers:blank",
+		"mynumbers:bracket_r","mynumbers:bracket_l","mynumbers:blank",
+		"mynumbers:square_bracket_r","mynumbers:square_bracket_l","mynumbers:curly_bracket_r",
+		"mynumbers:curly_bracket_l","mynumbers:ampersand","mynumbers:apostrophe","mynumbers:asterisk","mynumbers:at",
+		"mynumbers:back_slash","mynumbers:forward_slash","mynumbers:caret","mynumbers:colon","mynumbers:dollar","mynumbers:exclamation",
+		"mynumbers:hash","mynumbers:comma","mynumbers:percent","mynumbers:period","mynumbers:question_mark","mynumbers:quote",
+		"mynumbers:semi_colon","mynumbers:tilde"
 	}
 	
 	for _, entry in ipairs(mynumbers.colors) do
@@ -48,7 +53,7 @@ if core.get_modpath("mypaint") then
 		local desc = entry[2]
 		local paint = "^[colorize:"..entry[3]..":200"
 		
-	local num = {"0","1","2","3","4","5","6","7","8","9","plus","minus","multiply","divide","equals","bracket_r","bracket_l","slash","question","blank"}
+	local num = {"0","1","2","3","4","5","6","7","8","9","plus","minus","multiply","divide","equals","bracket_r","bracket_l","blank","square_bracket_r","square_bracket_l","curly_bracket_r","curly_bracket_l","ampersand","apostrophe","asterisk","at","back_slash","forward_slash","caret","colon","dollar","exclamation","hash","comma","percent","period","question_mark","quote","semi_colon","tilde"}
 		
 		for _, a in pairs(num) do
 			core.register_node("mynumbers:"..a.."_"..color,{
